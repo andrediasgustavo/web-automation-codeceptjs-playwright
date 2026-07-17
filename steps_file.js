c7023b60977b1ca8de78957ce4f5cdd9eac3d566
Andre Dias <andrediasgustavo@gmail.com>
2026-07-17 18:00:21 -0300

    ci: fix steps_file to avoid require() of ESM; use dynamic import only

--- steps_file.js
+++ steps_file.js
@@ -1,5 +1,4 @@
 // in this file you can append custom step methods to 'I' object
-const { actor } = require('codeceptjs');
 
 module.exports = async function() {
   const mod = await import('codeceptjs');
